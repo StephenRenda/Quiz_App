026d4a81e3d9a043b237bd889397c10a1d4fe79e
stephenrenda@csus.edu <stephenrenda@csus.edu>
2020-06-03 12:26:31 -0700

    Quiz with hook setState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Progress from "./components/Progress";
+import Question from "./components/Question";
+import Answers from "./components/Answers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentAnswer, setCurrentAnswer] = useState("");
+  const [answers, setAnswers] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [error, setError] = useState("");
+
+  const questions = [
+    {
+      id: 1,
+      question: "Which statement about Hooks is not true?",
+      answer_a:
+        "Hooks are 100% backwards-compatible and can be used side by side with classes",
+      answer_b: "Hooks are still in beta and not available yet",
+      answer_c:
+        "Hooks are completely opt-in, there's no need to rewrite existing code",
+      answer_d: "All of the above",
+      correct_answer: "b",
+    },
+    {
+      id: 2,
+      question: "Which one is not a Hook?",
+      answer_a: "useState()",
+      answer_b: "useConst()",
+      answer_c: "useReducer()",
+      answer_d: "All of the above",
+      correct_answer: "b",
+    },
+    {
+      id: 3,
+      question: "What Hook should be used for data fetching?",
+      answer_a: "useDataFetching()",
+      answer_b: "useApi()",
+      answer_c: "useEffect()",
+      answer_d: "useRequest()",
+      correct_answer: "c",
+    },
+  ];
+
+  const question = questions[currentQuestion];
+
+  const handleClick = (e) => {
+    setCurrentAnswer(e.target.value);
+    setError("");
+  };
+
+  const renderError = () => {
+    if (!error) {
+      return;
+    }
+
+    return <div className="error">{error}</div>;
+  };
+
+  const renderResultMark = (question, answer) => {
+    if (question.correct_answer === answer.answer) {
+      return <span className="correct">Correct</span>;
+    }
+    return <span className="failed">Incorrect</span>;
+  };
+
+  const renderResultData = () => {
+    return answers.map((answer) => {
+      const question = questions.find(
+        (question) => question.id === answer.questionId
+      );
+
+      return (
+        <div key={question.id}>
+          {question.question} - {renderResultMark(question, answer)}
+        </div>
+      );
+    });
+  };
+
+  const restart = () => {
+    setAnswers([]);
+    setCurrentAnswer("");
+    setCurrentQuestion(0);
+    setShowResults(false);
+  };
+
+  const next = () => {
+    const answer = { questionId: question.id, answer: currentAnswer };
+
+    if (!currentAnswer) {
+      setError("Please setect an answer.");
+      return;
+    }
+    answers.push(answer);
+    setAnswers(answers);
+    setCurrentAnswer("");
+    console.log(currentAnswer);
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+      return;
+    }
+    setShowResults(true);
+  };
+
+  if (showResults) {
+    return (
+      <div className="container results">
+        <h2>Results</h2>
+        <ul>{renderResultData()}</ul>
+        <button className="btn btn-primary" onClick={restart}>
+          Restart
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <Progress total={questions.length} current={currentQuestion + 1} />
+        <Question question={question.question} />
+        {renderError()}
+        <Answers
+          question={question}
+          currentAnswer={currentAnswer}
+          handleClick={handleClick}
+        />
+        <button className="btn btn-primary" onClick={next}>
+          Confirm and Continue
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
