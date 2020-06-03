68e3e05acdc6a0f9d79a33c9840852ced69c5e49
stephenrenda@csus.edu <stephenrenda@csus.edu>
2020-06-03 10:26:57 -0700

    Updates

---
+++ precache-manifest.cd8dbb500e5cdaacd4374857f7d9f044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379db76ccd75d5d16b0e369540c5d46e",
+    "url": "/Quiz_App/index.html"
+  },
+  {
+    "revision": "edefb610eaa2dcbba9bd",
+    "url": "/Quiz_App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2147d08dcff2d611c15d",
+    "url": "/Quiz_App/static/js/2.926b8d0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quiz_App/static/js/2.926b8d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edefb610eaa2dcbba9bd",
+    "url": "/Quiz_App/static/js/main.7d9a9241.chunk.js"
+  },
+  {
+    "revision": "4de75fb0881d851d39a5",
+    "url": "/Quiz_App/static/js/runtime-main.2887c4df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Quiz_App/static/media/logo.5d5d9eef.svg"
+  }
+]);
